5177feac7c96d1b270da7bb064341f14b3fc54b8
hacklover <info@hacklover.net>
2020-09-19 05:01:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Terminal module for OWD Client
+> A terminal for your Open Web Desktop client
+
+<p>
+    <a href="https://github.com/owdproject/owd-client/blob/master/LICENSE"><img src="https://img.shields.io/badge/license-MIT-green.svg" /></a>
+    <a href="https://github.com/owdproject/owd-client"><img src="https://img.shields.io/badge/owd-client-3A9CB6?logo=vue" /></a>
+    <a href="https://hacklover.net/patreon"><img src="https://img.shields.io/badge/become-a%20patron-orange" alt="Become a Patron" /></a>
+    <a href="https://hacklover.net/discord"><img src="https://img.shields.io/badge/chat-on%20discord-7289da.svg" alt="Join us on Discord" /></a>
+</p>
+
+## Demo
+[Try it out](https://hacklover.net), just click on the console icon on the left menu
+
+## Overview
+It's based on jQuery Terminal Emulator because I use this library from the beginning.
+Open Web Desktop is designed to support multiple terminals, other libraries will be implemented later.
+
+## Quick install
+- Copy the content of the `client` module folder into `owd-client/src/modules/terminal`
+- Add the reference of this module into `owd-client/config/modules.json` 
+
+## Add commands
+This feature isn't provided by OWD Terminal but is already present in Open Web Desktop.
+You can add commands to any module adding `"commands": true` in `module.json` and providing a `commands.js` file like this:
+
+```js
+export default ({store, terminal}) => {
+  return {
+    'test': function (...args) {  
+      this.echo('it works bruh')
+    }
+  }
+})
+```
+
+## License
+This project is released under the [MIT License](LICENSE).
---
+++ windows/WindowConsole.vue
@@ -0,0 +1,182 @@
+<template>
+  <Window :title="data.title" :window="data">
+    <div id="console" class="terminal" />
+  </Window>
+</template>
+
+<script>
+  // jQuery + Vue? a facc ro cazz
+  window.jQuery = require('jquery');
+
+  // jCubic Terminal integration
+  require('jquery.terminal');
+  import 'jquery.terminal/css/jquery.terminal.min.css'
+
+  import Window from "../../../core/components/window/Window";
+
+  export default {
+    name: "WindowConsole",
+    components: {Window},
+    props: {
+      data: Object
+    },
+    watch: {
+      // when window opens or closes
+      'data.storage.closed': function (val) {
+        if (val === true) {
+          this.$terminal.exec('reset')
+        }
+      }
+    },
+
+    mounted() {
+      const terminalElement = window.jQuery('#console');
+
+      const terminalInstance = terminalElement.terminal(
+          this.$terminal.commands,
+          {
+            greetings: this.$terminal.greetings,
+            prompt: '> ',
+            checkArity: false,
+            onCommandNotFound: () => false,
+            keydown: () => {
+              if (this.$terminal.events && this.$terminal.events.hasOwnProperty('keydown')) {
+                // run each keydown event
+                if (this.$terminal.events['keydown'].length > 0) {
+                  this.$terminal.events['keydown'].forEach(event => event())
+                }
+              }
+            }
+          }
+        );
+
+      this.$terminal.setInstance(terminalInstance);
+    },
+    destroyed() {
+      this.$terminal.destroy()
+    }
+  }
+</script>
+
+<style lang="scss">
+  #app.is-windows {
+    font-size: 14px;
+
+    #window-console {
+      .terminal {
+        .cmd,
+        .cmd div,
+        .cmd span:not(.token):not(.emoji),
+        .terminal,
+        .terminal-output>:not(.raw) a,
+        .terminal-output>:not(.raw) div,
+        .terminal-output>:not(.raw) span:not(.token):not(.inverted):not(.error):not(.emoji) {
+          font-size: 14px;
+        }
+      }
+    }
+  }
+
+  #window-console {
+    .v-progress-linear {
+      top: initial;
+    }
+
+    .terminal {
+      height: 100%;
+      padding: 0;
+      background: none;
+      user-select: text;
+
+      // spartan fix more width in console from mobile
+      @media (max-width: 560px) {
+        margin-right: -22%;
+      }
+      @media (max-width: 450px) {
+        margin-right: -24.5%;
+      }
+      @media (max-width: 411px) {
+        margin-right: -24%;
+      }
+      @media (max-width: 375px) {
+        margin-right: -25%;
+      }
+      @media (max-width: 360px) {
+        margin-right: -30%;
+      }
+
+      .cmd {
+        margin: 0 0 2px 0;
+
+        .cursor span span {
+          width: 6px;
+          height: 13px;
+          margin: -1px 0 0 1px;
+        }
+      }
+
+      &::-webkit-scrollbar {
+        width: 8px;
+        background: rgba(34, 34, 34, 0.3);
+      }
+
+      &::-webkit-scrollbar-thumb {
+        width: auto;
+        height: auto;
+        padding: 4px;
+        margin: 4px;
+        background: rgba(51, 51, 51, 0.3);
+      }
+
+      .cmd,
+      .cmd div,
+      .cmd span:not(.token):not(.emoji),
+      .terminal,
+      .terminal-output > :not(.raw) a,
+      .terminal-output > :not(.raw) div,
+      .terminal-output > :not(.raw) span:not(.token):not(.inverted):not(.error):not(.emoji) {
+        font-family: monospace, FreeMono;
+        background: none;
+
+        @media (max-width: 560px) {
+          font-size: 10.5px;
+          letter-spacing: -0.2px;
+        }
+
+        @media (max-width: 361px) {
+          letter-spacing: -0.5px;
+        }
+      }
+
+      .terminal-output {
+        > :not(.raw) a[href] {
+          color: #63696f;
+        }
+
+        > :not(.raw) a[href]:hover {
+          background-color: #63696f;
+        }
+      }
+
+      span[data-text="LIVE"] {
+        position: relative;
+        padding-left: 13px;
+
+        &:before {
+          position: absolute;
+          top: 2px;
+          left: 0;
+          width: 9px;
+          height: 9px;
+          border-radius: 50%;
+          background: #f5333d !important;
+          content: '';
+        }
+      }
+
+      .cursor.blink {
+        background: none;
+      }
+    }
+  }
+</style>
